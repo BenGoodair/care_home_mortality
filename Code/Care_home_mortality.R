fc5f86a644167bae580cb020b5765521ff378e4c
Ben Goodair <ben.goodair@outlook.com>
2025-05-15 14:22:47 +0100

    come on baby

--- Code/Care_home_mortality.R
+++ Code/Care_home_mortality.R
@@ -1,1 +1,12 @@
 ####Analysis 1 = Ecological####
+
+if (!require("pacman")) install.packages("pacman")
+
+pacman::p_load(devtools,np,lazyeval, hmisc,interp, lmtest,gt, modelsummary, dplyr,pdftools, tidyverse,rattle,glmnet,caret, rpart.plot, RcolorBrewer,rpart, tidyr, mice, stringr,randomForest,  curl, plm, readxl, zoo, stringr, patchwork,  sf, clubSandwich, modelsummary, sjPlot)
+
+
+
+
+
+
+
