2c1ba9fc7942f9028d7001e514966bb3a1b4f32a
Ben Goodair <ben.goodair@outlook.com>
2025-06-12 19:41:22 +0100

    iconic

--- Code/Care_home_mortality.R
+++ Code/Care_home_mortality.R
@@ -5,7 +5,56 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(devtools,np,lazyeval, hmisc,interp, lmtest,gt, modelsummary, dplyr,pdftools, tidyverse,rattle,glmnet,caret, rpart.plot, RcolorBrewer,rpart, tidyr, mice, stringr,randomForest,  curl, plm, readxl, zoo, stringr, patchwork,  sf, clubSandwich, modelsummary, sjPlot)
 
 
-outsourced <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/refs/heads/main/activity.csv"))
+outsourced <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/adults_social_care_data/refs/heads/main/activity.csv"))%>%
+  dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+  dplyr::select(DH)
+
+
+outsourced_spend <- read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+  dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+  dplyr::filter(Sector == "External",
+                SupportSetting == "Home care"|SupportSetting == "home care")%>%
+  dplyr::select(DH_GEOGRAPHY_NAME, year,percent_sector )%>%
+  dplyr::rename(outsourced_home_care = percent_sector)%>%
+  dplyr::full_join(.,
+                   read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+                     dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+                     dplyr::filter(Sector == "External",
+                                   SupportSetting == "Residential care home placements")%>%
+                     dplyr::select(DH_GEOGRAPHY_NAME, year,percent_sector )%>%
+                     dplyr::rename(outsourced_res_care = percent_sector))%>%
+  dplyr::full_join(.,
+                   read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+                     dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+                     dplyr::filter(Sector == "External",
+                                   SupportSetting == "Total over 65")%>%
+                     dplyr::select(DH_GEOGRAPHY_NAME, year,percent_sector )%>%
+                     dplyr::rename(outsourced_old_care = percent_sector))
+
+all_spend <- read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+  dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+  dplyr::filter(Sector == "Total",
+                SupportSetting == "Home care"|SupportSetting == "home care")%>%
+  dplyr::select(DH_GEOGRAPHY_NAME, year,Expenditure )%>%
+  dplyr::rename(spend_home_care = Expenditure)%>%
+  dplyr::full_join(.,
+                   read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+                     dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+                     dplyr::filter(Sector == "Total",
+                                   SupportSetting == "Residential care home placements")%>%
+                     dplyr::select(DH_GEOGRAPHY_NAME, year,Expenditure )%>%
+                     dplyr::rename(spend_res_care = Expenditure))%>%
+  dplyr::full_join(.,
+                   read.csv("Library/CloudStorage/OneDrive-Nexus365/Documents/GitHub/GitHub_new/adults_social_care_data/Final_data/expenditure.csv")%>%
+                     dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME)))%>%
+                     dplyr::filter(Sector == "Total",
+                                   SupportSetting == "Total over 65")%>%
+                     dplyr::select(DH_GEOGRAPHY_NAME, year,Expenditure )%>%
+                     dplyr::rename(spend_old_care = Expenditure))
+
+
+
+
 
 lifeexpectancy <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/care_home_mortality/refs/heads/main/Data/lifeexpectancylocalareas.csv"), skip=5)%>%
   dplyr::filter(Area.type == "County"|
@@ -57,8 +106,7 @@ deaths <- read.csv(curl("https://raw.githubusercontent.com/BenGoodair/care_home_
                   str_trim())
 
 
-data = dplyr::full_join(outsourced %>%
-                          dplyr::mutate(DH_GEOGRAPHY_NAME = str_trim(gsub("[0-9]", "", DH_GEOGRAPHY_NAME))), lifeexpectancy, by=c("DH_GEOGRAPHY_NAME", "year"))%>%
+data = dplyr::full_join(outsourced , lifeexpectancy, by=c("DH_GEOGRAPHY_NAME", "year"))%>%
   tidyr::drop_na(Life.expectancy..years., percent_sector)%>%
   dplyr::filter(DH_GEOGRAPHY_NAME!= "BOURNEMOUTH CHRISTCHURCH AND POOLE",
                 DH_GEOGRAPHY_NAME!= "BEDFORD",
@@ -89,11 +137,121 @@ duplicated_rows <- data %>%
   dplyr::group_by(DH_GEOGRAPHY_NAME, year) %>%
   dplyr::filter(n() > 1)
 
+carehomedata <-  read.csv("~/Library/CloudStorage/OneDrive-Nexus365/Documents/Children's Care Homes Project/CQC_API_Materials/Data/complete inspection and location data_ben_feb2025v2.csv")%>%
+  dplyr::select(closed_complete, locationlocalauthority, location_end_2025)%>%
+  dplyr::filter(closed_complete==1)%>%
+  mutate(
+    closure_date = as.Date(location_end_2025, origin = "1960-01-01"),
+    year = isoyear(closure_date),
+    week = isoweek(closure_date)
+  ) %>%
+  group_by(locationlocalauthority, year, week) %>%
+  summarise(n_closures = n(), .groups = "drop")%>%
+  dplyr::ungroup()%>%
+  dplyr::mutate(DH_GEOGRAPHY_NAME  = locationlocalauthority %>%
+                  gsub('&', 'and', .) %>%
+                  gsub('[[:punct:] ]+', ' ', .) %>%
+                  gsub('[0-9]', '', .)%>%
+                  toupper() %>%
+                  gsub("CITY OF", "",.)%>%
+                  gsub("UA", "",.)%>%
+                  gsub("COUNTY OF", "",.)%>%
+                  gsub("ROYAL BOROUGH OF", "",.)%>%
+                  gsub("LEICESTER CITY", "LEICESTER",.)%>%
+                  gsub("UA", "",.)%>%
+                  gsub("DARWIN", "DARWEN", .)%>%
+                  gsub("COUNTY DURHAM", "DURHAM", .)%>%
+                  gsub("AND DARWEN", "WITH DARWEN", .)%>%
+                  gsub("NE SOM", "NORTH EAST SOM", .)%>%
+                  gsub("N E SOM", "NORTH EAST SOM", .)%>%
+                  str_trim())%>%
+  dplyr::select(-locationlocalauthority)
+
+all_combinations <- expand_grid(
+  DH_GEOGRAPHY_NAME = unique(carehomedata$DH_GEOGRAPHY_NAME),
+  year = min(carehomedata$year):max(carehomedata$year),
+  week = 1:53  # ISO weeks can go up to 53
+)
+
+# Join with existing data and fill missing with 0
+complete_data <- all_combinations %>%
+  left_join(carehomedata, by = c("DH_GEOGRAPHY_NAME", "year", "week")) %>%
+  mutate(n_closures = replace_na(n_closures, 0))
+
+  
+  deaths <- read.csv
+  
+  
+  
+  
+# Define full range of years and all LAs
+all_years <- 2011:2024
+all_LAs <- unique(carehomedata$DH_GEOGRAPHY_NAME)
+
+# Create a complete grid of all year–LA combinations
+complete_grid <- expand_grid(
+  year_location_end_2025 = all_years,
+  DH_GEOGRAPHY_NAME = all_LAs
+)
+
+# Join with the original data and fill NAs with 0
+completed_data <- complete_grid %>%
+  left_join(carehomedata, by = c("year_location_end_2025", "DH_GEOGRAPHY_NAME")) %>%
+  mutate(closed_complete = replace_na(closed_complete, 0)) %>%
+  arrange(DH_GEOGRAPHY_NAME, year_location_end_2025)%>%
+  dplyr::rename(year = year_location_end_2025)%>%
+  dplyr::group_by(DH_GEOGRAPHY_NAME)%>%
+  arrange(year) %>%
+  dplyr::mutate(lagged_closed = dplyr::lag(closed_complete, 1))
 
 
-summary(plm(log(Life.expectancy..years._Male)~ percent_sector+lagged_sector_1+lagged_sector_2+lagged_le_1_Male, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
-summary(plm(log(Life.expectancy..years._Female)~  percent_sector+lagged_sector_1+lagged_sector_2+lagged_le_1_Female, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
-summary(plm(log(deaths.All.care.homes)~ percent_sector+lagged_sector_1+lagged_sector_2+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+
+data <- data %>%
+  dplyr::full_join(., completed_data)
+
+data <- data %>% 
+  dplyr::full_join(., outsourced_spend)
+
+data <- data %>% 
+  dplyr::full_join(., all_spend)
+
+
+data = data %>%
+  dplyr::group_by(DH_GEOGRAPHY_NAME)%>%
+  arrange(year) %>%
+  dplyr::mutate(lagged_home_out = dplyr::lag(outsourced_home_care, 1),
+                lagged_home_spend = dplyr::lag(spend_home_care, 1),
+                lagged_res_out = dplyr::lag(outsourced_res_care, 1),
+                lagged_res_spend = dplyr::lag(spend_res_care, 1),
+                lagged_old_out = dplyr::lag(outsourced_old_care, 1),
+                lagged_old_spend = dplyr::lag(spend_old_care, 1))
+
+
+summary(plm(log(Life.expectancy..years._Male)~ lagged_sector_1+lagged_le_1_Male, data= data%>%dplyr::filter(percent_sector>0), index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~  lagged_sector_1+lagged_le_1_Female, data= data%>%dplyr::filter(percent_sector>0), index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes)~ lagged_sector_1+lagged_deaths.All.care.homes, data= data%>%dplyr::filter(percent_sector>0), index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes)~ lagged_closed+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.non.LA.care.homes)~ lagged_closed+deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.LA.care.homes+1)~ lagged_closed+deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~ lagged_closed+lagged_le_1_Female, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Male)~ lagged_closed+lagged_le_1_Male, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+
+summary(plm(log(Life.expectancy..years._Male)~ lagged_home_out+lagged_home_spend+lagged_le_1_Male, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~ lagged_home_out+lagged_home_spend+lagged_le_1_Female, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes+1)~ lagged_home_out+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+
+
+summary(plm(log(Life.expectancy..years._Male)~ lagged_home_out+lagged_home_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~ lagged_home_out+lagged_home_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes+1)~ lagged_home_out+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Male)~ lagged_old_out+lagged_res_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~ lagged_old_out+lagged_res_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes+1)~ lagged_old_out+lagged_res_spend+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Male)~ lagged_res_out+lagged_res_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(Life.expectancy..years._Female)~ lagged_res_out+lagged_res_spend, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+summary(plm(log(deaths.All.care.homes+1)~ lagged_res_out+lagged_res_spend+lagged_deaths.All.care.homes, data= data, index = c("DH_GEOGRAPHY_NAME", "year"), effect = "twoways", model = "within"))
+
+
 
 library(plm)
 library(dplyr)
